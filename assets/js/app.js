f88c80698d01c08688b23d4d6fa618b852eda457
AReaver <secre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.nameAReaversecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.emailzailancocmail.comsecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global --unset user.namesecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global --unset user.emailsecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.name AReaversecre@DESKTOP-6TGC9OM MINGW64 ~ (master)$ git config --global user.email>
2023-11-29 03:16:29 -0600

    Se sube repositorio de actividad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,35 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); // Changed selector to '.name'
+const $b = document.querySelector('.blog'); // Changed selector to '.blog'
+const $l = document.querySelector('.location'); // Changed selector to '.location'
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) { // Added 'async' keyword
+    try {
+        $n.textContent = 'Cargando...';
+        const response = await fetch(`${usersEndpoint}/${username}`);
+        const data = await response.json(); // Parsing response JSON
+        console.log(data);
+        $n.textContent = data.name || 'Nombre no disponible'; // Using data.name or providing a default message
+        $b.textContent = data.blog || 'Blog no disponible'; // Using data.blog or providing a default message
+        $l.textContent = data.location || 'Ubicación no disponible'; // Using data.location or providing a default message
+    } catch (err) {
+        handleError(err);
+    }
 }
 
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+    console.log('OH NO!');
+    console.log(err);
+    $n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
+
+
+
+/* Cambié los selectores del DOM para obtener los elementos por su clase correspondiente: querySelector('.name'), querySelector('.blog'), y querySelector('.location').
+Añadí la palabra clave async a la función displayUser para utilizar await en las llamadas asíncronas.
+Añadí un bloque try...catch para manejar los errores de la petición a la API.
+Parseé la respuesta de la API a JSON usando response.json().
+Usé los datos obtenidos para mostrar el nombre, blog y ubicación del usuario en el DOM, o proporcionar un mensaje predeterminado si la información no está disponible. */
